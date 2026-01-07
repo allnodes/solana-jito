63adfe1ddedf85ef7eef0f24eb293056a3b658cd
Sephiroth <sephiroth@abyss.finance>
2026-01-07 09:26:10 +0000

    Allnodes Patch

--- local-cluster/src/validator_configs.rs
+++ local-cluster/src/validator_configs.rs
@@ -86,6 +86,16 @@ pub fn safe_clone_config(config: &ValidatorConfig) -> ValidatorConfig {
         shred_receiver_address: config.shred_receiver_address.clone(),
         shred_retransmit_receiver_address: config.shred_retransmit_receiver_address.clone(),
         tip_manager_config: config.tip_manager_config.clone(),
+
+        // Allnodes configuration
+        identity_path: config.identity_path.clone(),
+        use_mostly_confirmed_threshold: config.use_mostly_confirmed_threshold,
+        mostly_confirmed_threshold_config_path: config
+            .mostly_confirmed_threshold_config_path
+            .clone(),
+        voting_patch_flags: config.voting_patch_flags,
+        voting_patch_flags2: config.voting_patch_flags2.clone(),
+        poh_message: config.poh_message.clone(),
     }
 }
 
