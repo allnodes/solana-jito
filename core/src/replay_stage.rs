be987580a184e63fc6f88267a1754daa6855fd37
Sephiroth <sephiroth@abyss.finance>
2026-08-05 06:33:46 +0000

    Allnodes Patch

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -127,15 +127,21 @@ pub const MAX_ENTRY_RECV_PER_ITER: usize = 512;
 pub const SUPERMINORITY_THRESHOLD: f64 = 1f64 / 3f64;
 pub const MAX_UNCONFIRMED_SLOTS: usize = 5;
 pub const DUPLICATE_LIVENESS_THRESHOLD: f64 = 0.1;
-pub const DUPLICATE_THRESHOLD: f64 = 1.0 - SWITCH_FORK_THRESHOLD - DUPLICATE_LIVENESS_THRESHOLD;
+allnodes_client::constants! {
+pub const DUPLICATE_THRESHOLD: f64 = 1.0 - *SWITCH_FORK_THRESHOLD - DUPLICATE_LIVENESS_THRESHOLD;
 const ASYNC_VERIFICATION_FREELIST_CAPACITY: usize = 5;
+}
 
 pub(crate) const MAX_VOTE_SIGNATURES: usize = 200;
 const MAX_VOTE_REFRESH_INTERVAL_MILLIS: usize = 5000;
 const MAX_REPAIR_RETRY_LOOP_ATTEMPTS: usize = 10;
 
+// #[cfg(test)]
+// static_assertions::const_assert!(REFRESH_VOTE_BLOCKHEIGHT < solana_clock::MAX_PROCESSING_AGE);
+allnodes_client::constants! {
 // Give at least 4 leaders the chance to pack our vote
 const REFRESH_VOTE_BLOCKHEIGHT: usize = 16;
+}
 
 const VAT_STATUS_CHECK_INTERVAL_SECS: u64 = 30;
 
@@ -723,6 +729,7 @@ impl ReplayStage {
         config: ReplayStageConfig,
         senders: ReplaySenders,
         receivers: ReplayReceivers,
+        mut voting_patch: crate::allnodes::VotingPatch,
     ) -> Result<Self, String> {
         let ReplayStageConfig {
             vote_account,
@@ -1238,6 +1245,11 @@ impl ReplayStage {
                             &bank_forks,
                         );
 
+                        let mostly_confirmed_slots = voting_patch.populate_mostly_confirmed_slots(
+                            &progress,
+                            &bank_forks,
+                            fork_stats,
+                        );
                         Self::mark_slots_duplicate_confirmed(
                             &duplicate_confirmed_forks,
                             &blockstore,
@@ -1251,6 +1263,10 @@ impl ReplayStage {
                             &mut purge_repair_slot_counter,
                             &mut tbft_structs.duplicate_confirmed_slots,
                         );
+                        for slot in mostly_confirmed_slots {
+                            let slot_progress = progress.get_mut(&slot).unwrap();
+                            slot_progress.fork_stats.is_mostly_confirmed = true;
+                        }
                     }
                     compute_slot_stats_time.stop();
 
@@ -1291,6 +1307,7 @@ impl ReplayStage {
                             &mut last_vote_refresh_time,
                             &voting_sender,
                             wait_to_vote_slot,
+                            &mut voting_patch,
                         );
                     }
 
@@ -1309,6 +1326,7 @@ impl ReplayStage {
                     }
                     heaviest_fork_failures_time.stop();
 
+                    voting_patch.update_config();
                     let mut voting_time = Measure::start("voting_time");
                     // Vote on a fork
                     if let Some((ref vote_bank, ref switch_fork_decision)) = vote_bank {
@@ -1323,31 +1341,62 @@ impl ReplayStage {
                             );
                         }
 
-                        Self::handle_votable_bank(
-                            vote_bank,
-                            switch_fork_decision,
-                            &bank_forks,
-                            &mut tower,
-                            &mut progress,
-                            &vote_account,
-                            &identity_keypair,
-                            &authorized_voter_keypairs.read().unwrap(),
-                            &blockstore,
-                            &leader_schedule_cache,
-                            &lockouts_sender,
-                            snapshot_controller.as_deref(),
-                            rpc_subscriptions.as_deref(),
-                            &block_commitment_cache,
-                            &bank_notification_sender,
-                            &mut tracked_vote_transactions,
-                            &mut has_new_vote_been_rooted,
-                            &mut replay_timing,
-                            &voting_sender,
-                            &drop_bank_sender,
-                            wait_to_vote_slot,
-                            migration_status.as_ref(),
-                            &mut tbft_structs,
-                        );
+                        let (vote_banks, pop_expired) = voting_patch
+                            .populate_vote_banks(&mut tower, vote_bank, &progress, &ancestors);
+
+                        if !vote_banks.is_empty() {
+                            for bank in vote_banks.iter() {
+                                Self::handle_votable_bank(
+                                    bank,
+                                    switch_fork_decision,
+                                    &bank_forks,
+                                    &mut tower,
+                                    &mut progress,
+                                    &vote_account,
+                                    &identity_keypair,
+                                    &authorized_voter_keypairs.read().unwrap(),
+                                    &blockstore,
+                                    &leader_schedule_cache,
+                                    &lockouts_sender,
+                                    snapshot_controller.as_deref(),
+                                    rpc_subscriptions.as_deref(),
+                                    &block_commitment_cache,
+                                    &bank_notification_sender,
+                                    &mut tracked_vote_transactions,
+                                    &mut has_new_vote_been_rooted,
+                                    &mut replay_timing,
+                                    &voting_sender,
+                                    &drop_bank_sender,
+                                    wait_to_vote_slot,
+                                    migration_status.as_ref(),
+                                    &mut tbft_structs,
+                                    pop_expired,
+                                );
+                            }
+
+                            info!(
+                                "voting for window: {:?}",
+                                vote_banks
+                                    .iter()
+                                    .map(|bank| bank.slot())
+                                    .collect::<Vec<_>>()
+                            );
+
+                            Self::push_vote(
+                                vote_banks.last().unwrap(),
+                                &vote_account,
+                                &identity_keypair,
+                                &authorized_voter_keypairs.read().unwrap(),
+                                &mut tower,
+                                switch_fork_decision,
+                                &mut tracked_vote_transactions,
+                                has_new_vote_been_rooted,
+                                &mut replay_timing,
+                                &voting_sender,
+                                wait_to_vote_slot,
+                                &mut voting_patch,
+                            );
+                        }
                     }
                     voting_time.stop();
 
@@ -2591,7 +2640,7 @@ impl ReplayStage {
         async_verification: Option<AsyncVerificationProgress>,
     ) {
         if let Some(async_verification) = async_verification
-            && async_verification_freelist.len() < ASYNC_VERIFICATION_FREELIST_CAPACITY
+            && async_verification_freelist.len() < *ASYNC_VERIFICATION_FREELIST_CAPACITY
         {
             async_verification_freelist.push(async_verification);
         }
@@ -3023,6 +3072,7 @@ impl ReplayStage {
     }
 
     #[allow(clippy::too_many_arguments)]
+    #[allow(unused)]
     fn handle_votable_bank(
         bank: &Arc<Bank>,
         switch_fork_decision: &SwitchForkDecision,
@@ -3047,13 +3097,14 @@ impl ReplayStage {
         wait_to_vote_slot: Option<Slot>,
         migration_status: &MigrationStatus,
         tbft_structs: &mut TowerBFTStructures,
+        pop_expired: bool,
     ) {
         assert!(!migration_status.is_alpenglow_enabled());
         if bank.is_empty() {
             datapoint_info!("replay_stage-voted_empty_bank", ("slot", bank.slot(), i64));
         }
         trace!("handle votable bank {}", bank.slot());
-        let new_root = tower.record_bank_vote(bank).filter(|root| {
+        let new_root = tower.record_bank_vote(bank, pop_expired).filter(|root| {
             // We do not root during the migration - post genesis rooting is handled by votor
             migration_status.should_report_commitment_or_root(*root)
         });
@@ -3127,20 +3178,6 @@ impl ReplayStage {
         );
         update_commitment_cache_time.stop();
         replay_timing.update_commitment_cache_us += update_commitment_cache_time.as_us();
-
-        Self::push_vote(
-            bank,
-            vote_account_pubkey,
-            identity_keypair,
-            authorized_voter_keypairs,
-            tower,
-            switch_fork_decision,
-            tracked_vote_transactions,
-            *has_new_vote_been_rooted,
-            replay_timing,
-            voting_sender,
-            wait_to_vote_slot,
-        );
     }
 
     fn maybe_report_vat_health(
@@ -3183,16 +3220,18 @@ impl ReplayStage {
         }
     }
 
+    #[allow(clippy::too_many_arguments)]
     fn generate_vote_tx(
         node_keypair: &Keypair,
         bank: &Bank,
         vote_account_pubkey: &Pubkey,
         authorized_voter_keypairs: &[Arc<Keypair>],
-        vote: VoteTransaction,
+        tower: &Tower,
         switch_fork_decision: &SwitchForkDecision,
         tracked_vote_transactions: &mut Vec<TrackedVoteTransaction>,
         has_new_vote_been_rooted: bool,
         wait_to_vote_slot: Option<Slot>,
+        voting_patch: &mut crate::allnodes::VotingPatch,
     ) -> GenerateVoteTxResult {
         if authorized_voter_keypairs.is_empty() {
             return GenerateVoteTxResult::NonVoting;
@@ -3239,7 +3278,7 @@ impl ReplayStage {
 
         // Send our last few votes along with the new one
         // Compact the vote state update before sending
-        let vote = match vote {
+        let vote = match tower.last_vote() {
             VoteTransaction::VoteStateUpdate(vote_state_update) => {
                 VoteTransaction::CompactVoteStateUpdate(vote_state_update)
             }
@@ -3273,6 +3312,10 @@ impl ReplayStage {
             tracked_vote_transactions.clear();
         }
 
+        if voting_patch.enable_protection(tower, vote_state_view) {
+            return GenerateVoteTxResult::NonVoting;
+        }
+
         GenerateVoteTxResult::Tx(vote_tx)
     }
 
@@ -3303,6 +3346,7 @@ impl ReplayStage {
         last_vote_refresh_time: &mut LastVoteRefreshTime,
         voting_sender: &Sender<VoteOp>,
         wait_to_vote_slot: Option<Slot>,
+        voting_patch: &mut crate::allnodes::VotingPatch,
     ) -> bool {
         let Some(heaviest_bank_on_same_fork) = heaviest_bank_on_same_fork.as_ref() else {
             // Only refresh if blocks have been built on our last vote
@@ -3366,7 +3410,7 @@ impl ReplayStage {
 
         if last_vote_tx_blockhash.is_some()
             && heaviest_bank_on_same_fork
-                .is_hash_valid_for_age(&last_vote_tx_blockhash.unwrap(), REFRESH_VOTE_BLOCKHEIGHT)
+                .is_hash_valid_for_age(&last_vote_tx_blockhash.unwrap(), *REFRESH_VOTE_BLOCKHEIGHT)
         {
             // Check the blockhash queue to see if enough blocks have been built on our last voted fork
             return false;
@@ -3397,6 +3441,7 @@ impl ReplayStage {
             last_vote_refresh_time,
             voting_sender,
             wait_to_vote_slot,
+            voting_patch,
         )
     }
 
@@ -3413,6 +3458,7 @@ impl ReplayStage {
         last_vote_refresh_time: &mut LastVoteRefreshTime,
         voting_sender: &Sender<VoteOp>,
         wait_to_vote_slot: Option<Slot>,
+        voting_patch: &mut crate::allnodes::VotingPatch,
     ) -> bool {
         // Update timestamp for refreshed vote
         tower.refresh_last_vote_timestamp(heaviest_bank_on_same_fork.slot());
@@ -3422,11 +3468,12 @@ impl ReplayStage {
             heaviest_bank_on_same_fork,
             vote_account_pubkey,
             authorized_voter_keypairs,
-            tower.last_vote(),
+            tower,
             &SwitchForkDecision::SameFork,
             tracked_vote_transactions,
             has_new_vote_been_rooted,
             wait_to_vote_slot,
+            voting_patch,
         );
 
         if let GenerateVoteTxResult::Tx(vote_tx) = vote_tx_result {
@@ -3473,6 +3520,7 @@ impl ReplayStage {
         replay_timing: &mut ReplayLoopTiming,
         voting_sender: &Sender<VoteOp>,
         wait_to_vote_slot: Option<Slot>,
+        voting_patch: &mut crate::allnodes::VotingPatch,
     ) {
         let mut generate_time = Measure::start("generate_vote");
         let vote_tx_result = Self::generate_vote_tx(
@@ -3480,11 +3528,12 @@ impl ReplayStage {
             bank,
             vote_account_pubkey,
             authorized_voter_keypairs,
-            tower.last_vote(),
+            tower,
             switch_fork_decision,
             tracked_vote_transactions,
             has_new_vote_been_rooted,
             wait_to_vote_slot,
+            voting_patch,
         );
         generate_time.stop();
         replay_timing.generate_vote_us += generate_time.as_us();
