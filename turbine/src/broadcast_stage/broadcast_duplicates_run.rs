842d0532a451c7d1cadd9c4ab78451f3ff004d8f
Sephiroth <sephiroth@abyss.finance>
2026-09-08 19:19:26 +0000

    Allnodes Patch

--- turbine/src/broadcast_stage/broadcast_duplicates_run.rs
+++ turbine/src/broadcast_stage/broadcast_duplicates_run.rs
@@ -23,8 +23,10 @@ fn duplicate_shred_key(shred: &Shred) -> DuplicateShredKey {
     (*shred.signature(), shred.id())
 }
 
+allnodes_client::constants! {
 pub const MINIMUM_DUPLICATE_SLOT: Slot = 20;
 pub const DUPLICATE_RATE: usize = 10;
+}
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub enum ClusterPartition {
@@ -145,8 +147,8 @@ impl BroadcastRun for BroadcastDuplicatesRun {
         // in the slot to make verification fail on validators
         let last_entries = {
             if last_tick_height == bank.max_tick_height()
-                && bank.slot() > MINIMUM_DUPLICATE_SLOT
-                && self.num_slots_broadcasted.is_multiple_of(DUPLICATE_RATE)
+                && bank.slot() > *MINIMUM_DUPLICATE_SLOT
+                && self.num_slots_broadcasted.is_multiple_of(*DUPLICATE_RATE)
                 && let Some(recent_blockhash) = self.recent_blockhash
             {
                 let entry_batch_len = entries.len();
